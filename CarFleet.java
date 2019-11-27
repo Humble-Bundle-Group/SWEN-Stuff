bdde1c2fe88d1e922277253ae0cdb87bc8662698
jholung12 <55642177+jholung12@users.noreply.github.com>
2019-11-27 14:32:59 -0500

    Added CarFleet

---
+++ CarFleet.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class CarFleet{
+  private ArrayList<Vehicle> list;
+
+  public CarFleet(){
+    this.list = new ArrayList<Vehicle>();
+  }
+
+  public void addVehicle(Vehicle vehicle){
+    list.add(vehicle);
+  }
+
+  public int checkList(String id){
+    int i;
+
+    for(i=0; i< list.size(); i++){
+      if(list.get(i).getVin() == id) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  public void removeVehicle(String id){
+    int i = checkList(id);
+    if(i >= 0){
+      list.remove(i);
+    }
+    else{
+      System.out.println("ID could not be found.");
+    }
+  }
+
+  public Vehicle searchVehicle(String id){
+    int i = checkList(id);
+    Vehicle empty = new Vehicle("","","",-1,-1);
+
+    if (i >= 0){
+      return list.get(i);
+    }
+    else{
+      return empty;
+    }
+  }
+
+  public ArrayList<Vehicle> getList(){
+    return this.list;
+  }
+}
